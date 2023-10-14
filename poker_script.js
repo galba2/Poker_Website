c4686f7df5e2231dac8b9b7f770fa8a8bf8508d3
galba2 <gilbert.alba1110@gmail.com>
2023-10-14 01:27:39 -0500

    commit 73 +poker_script.js to game.html +test top styles to @keyframes cardFlick +test zero delay & iteration count to transform() +test player two's first card to elementId to transform()

--- poker_script.js
+++ poker_script.js
@@ -1,3 +1,3 @@
 function transform() {
-    document.getElementById("user-cards-first").style.animation = "cardFlick 3s linear 1"
+    document.getElementById("player-cards-two-first").style.animation = "cardFlick 1s linear 0s 1";
 }
