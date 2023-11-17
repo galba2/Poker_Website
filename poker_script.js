5981afb124f9231a07aed029aae16d5c3daa058b
galba2 <gilbert.alba1110@gmail.com>
2023-11-17 06:08:04 -0600

    commit 84 +javascript animation to player two second's card +percents to css for player two second's card

--- poker_script.js
+++ poker_script.js
@@ -1,4 +1,4 @@
 function buttonPressed() {
     document.getElementById("player-cards-two-first").style.animation = "cardFlickPlayerTwoFirst .7s linear 0s normal both";
-    document.getElementById("player-cards-two-second").style.animation
+    document.getElementById("player-cards-two-second").style.animation = "cardFlickPlayerTwoSecond .7s ease-out 0s normal both";
 }
