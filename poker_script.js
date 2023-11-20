4ed61669ba29916a59e957fe8d372aedef0dd77e
galba2 <gilbert.alba1110@gmail.com>
2023-11-19 18:21:32 -0600

    commit 85 +player two's card flick css animations to all cards +player two's card flick javascript connections to all cards

--- poker_script.js
+++ poker_script.js
@@ -1,4 +1,7 @@
 function buttonPressed() {
     document.getElementById("player-cards-two-first").style.animation = "cardFlickPlayerTwoFirst .7s linear 0s normal both";
     document.getElementById("player-cards-two-second").style.animation = "cardFlickPlayerTwoSecond .7s ease-out 0s normal both";
+    document.getElementById("player-cards-two-third").style.animation = "cardFlickPlayerTwoThird .7s linear 0s normal both";
+    document.getElementById("player-cards-two-fourth").style.animation = "cardFlickPlayerTwoFourth .7s ease-out 0s normal both";
+    document.getElementById("player-cards-two-fifth").style.animation = "cardFlickPlayerTwoFifth .7s ease-out 0s normal both";
 }
