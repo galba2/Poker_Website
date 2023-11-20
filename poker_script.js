85a15ce876d0290a0f70f8cc9bd7c75ad4880def
galba2 <gilbert.alba1110@gmail.com>
2023-11-20 05:44:44 -0600

    commit 86 +partial player three's javascript card flick connections

--- poker_script.js
+++ poker_script.js
@@ -1,7 +1,15 @@
 function buttonPressed() {
+    //PLAYER TWO CARDFLICK
     document.getElementById("player-cards-two-first").style.animation = "cardFlickPlayerTwoFirst .7s linear 0s normal both";
     document.getElementById("player-cards-two-second").style.animation = "cardFlickPlayerTwoSecond .7s ease-out 0s normal both";
     document.getElementById("player-cards-two-third").style.animation = "cardFlickPlayerTwoThird .7s linear 0s normal both";
     document.getElementById("player-cards-two-fourth").style.animation = "cardFlickPlayerTwoFourth .7s ease-out 0s normal both";
     document.getElementById("player-cards-two-fifth").style.animation = "cardFlickPlayerTwoFifth .7s ease-out 0s normal both";
+
+    //PLAYER THREE CARDFLICK
+    document.getElementById("player-cards-three-first").style.animation = "cardFlickPlayerTwoFirst .7s linear 0s normal both";
+    document.getElementById("player-cards-three-second").style.animation = "cardFlickPlayerTwoSecond .7s ease-out 0s normal both";
+    document.getElementById("player-cards-three-third").style.animation = "cardFlickPlayerTwoThird .7s linear 0s normal both";
+    document.getElementById("player-cards-three-fourth").style.animation = "cardFlickPlayerTwoFourth .7s ease-out 0s normal both";
+    document.getElementById("player-cards-three-fifth").style.animation = "cardFlickPlayerTwoFifth .7s ease-out 0s normal both";
 }
