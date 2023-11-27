5cbaa728a8996a1b3aee9b3808ed93e6f0ae4993
galba2 <gilbert.alba1110@gmail.com>
2023-11-27 06:13:17 -0600

    commit 87 +rename of player three's javascript connection +player three's animation to css

--- poker_script.js
+++ poker_script.js
@@ -7,9 +7,9 @@ function buttonPressed() {
     document.getElementById("player-cards-two-fifth").style.animation = "cardFlickPlayerTwoFifth .7s ease-out 0s normal both";
 
     //PLAYER THREE CARDFLICK
-    document.getElementById("player-cards-three-first").style.animation = "cardFlickPlayerTwoFirst .7s linear 0s normal both";
-    document.getElementById("player-cards-three-second").style.animation = "cardFlickPlayerTwoSecond .7s ease-out 0s normal both";
-    document.getElementById("player-cards-three-third").style.animation = "cardFlickPlayerTwoThird .7s linear 0s normal both";
-    document.getElementById("player-cards-three-fourth").style.animation = "cardFlickPlayerTwoFourth .7s ease-out 0s normal both";
-    document.getElementById("player-cards-three-fifth").style.animation = "cardFlickPlayerTwoFifth .7s ease-out 0s normal both";
+    document.getElementById("player-cards-three-first").style.animation = "cardFlickPlayerThreeFirst .7s linear 0s normal both";
+    document.getElementById("player-cards-three-second").style.animation = "cardFlickPlayerThreeSecond .7s ease-out 0s normal both";
+    document.getElementById("player-cards-three-third").style.animation = "cardFlickPlayerThreeThird .7s linear 0s normal both";
+    document.getElementById("player-cards-three-fourth").style.animation = "cardFlickPlayerThreeFourth .7s ease-out 0s normal both";
+    document.getElementById("player-cards-three-fifth").style.animation = "cardFlickPlayerThreeFifth .7s ease-out 0s normal both";
 }
